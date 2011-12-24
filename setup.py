42027aaef844c3d6ae16a2a20dde5a4ec5e51036
Gabriel Falcao <gabriel@nacaolivre.org>
2011-12-24 12:35:39 -0500

    bump to 0.3.5

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def get_packages():
     return packages
 
 setup(name='unclebob',
-    version='0.3.4',
+    version='0.3.5',
     description='Django testing tool set',
     author=u'Gabriel Falcao',
     author_email='gabriel@nacaolivre.org',
